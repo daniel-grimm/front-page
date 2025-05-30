a42aac60a71e1ab007822dca726a7c030758ba5b
Daniel Grimm <danielsgrimm@hotmail.com>
2025-05-29 22:09:13 -0500

    Updating the front page of the website with a more engaging and informative layout.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to My SvelteKit Experiment!</h1>
+<p>Hello, I'm a full stack developer based in the Dallas Fort Worth Metroplex.</p>
+A little about me:
+<ul>
+<li>Founder of this experimentation playground</li>
+<li>Full stack developer with experience in <a href="https://en.wikipedia.org/wiki/Web_development">web development</a></li>
+<li>Always looking for new ways to improve my skills and learn from others</li>
+</ul>
+
+<p>
+  Want to know more about me? Check out my portfolio at:
+  <a href="https://your-portfolio-url.com">Your Portfolio URL</a>
+</p>
+
+<p>
+  Visit 
+  <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
